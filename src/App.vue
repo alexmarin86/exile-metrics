44e36b1f5c42b569d3dbdab80639dfcf716ee3be
alexmarin86 <alexeu86@gmail.com>
2025-07-26 01:10:56 +0200

    init and some initial components

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import ThemeSwitcher from './components/ThemeSwitcher.vue'
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen bg-background">
+    <header
+      class="h-20 bg-primary-foreground supports-[backdrop-filter]:bg-primary-foreground/60 text-primary sticky top-0 backdrop-blur shadow-md shadow-primary-foreground/70"
+    >
+      <nav class="container mx-auto flex justify-between items-center h-full">
+        <ul class="flex gap-4">
+          <li><RouterLink class="" to="/">Home</RouterLink></li>
+          <li><RouterLink class="" to="/about">About</RouterLink></li>
+        </ul>
+        <ThemeSwitcher />
+      </nav>
+    </header>
+    <div class="grow py-8 text-foreground">
+      <RouterView />
+    </div>
+  </div>
+</template>
---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Sun, Moon, Monitor } from 'lucide-vue-next'
+import { useColorMode } from '@vueuse/core'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+// Pass { disableTransition: false } to enable transitions
+const mode = useColorMode()
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="secondary">
+        <Sun v-if="mode === 'light'" class="h-4 w-4" />
+        <Moon v-if="mode === 'dark'" class="h-4 w-4" />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="mode = 'light'">
+        <Sun class="h-4 w-4 mr-1" />
+        Light
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'dark'">
+        <Moon class="h-4 w-4 mr-1" />
+        Dark
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'auto'">
+        <Monitor class="h-4 w-4 mr-1" />
+        System
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
